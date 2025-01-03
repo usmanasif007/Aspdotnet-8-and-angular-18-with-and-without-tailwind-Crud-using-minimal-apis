03d17356293a2635e4eea66c8e603f91a36d086b
usmanasif007 <rappidz296@gmail.com>
2025-01-03 12:17:36 +0500

    changes

--- Backend/Assesment_ProductManagementSystem_Usman/Services/Product/DTOs/ProductFilterDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿using Assesment_ProductManagementSystem_Usman.CommonDTOs;
-
-namespace Assesment_ProductManagementSystem_Usman.Services.Product.DTOs
-{
-    public class ProductFilterDTO : PaginationDTO
-    {
-        public int Id { get; set; }
-    }
-}
